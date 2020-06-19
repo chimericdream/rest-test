482890130edc007e2581f5b76c95822215124540
Bill Parrott <william.parrott@e-hps.com>
2020-06-19 13:25:17 -0500

    minor changes

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const formidable = require('formidable');
 const app = express();
 
 app.use(bodyParser.json());
 
+app.post('/submit', (req, res) => {
+    new formidable.IncomingForm().parse(req, (err, fields, files) => {
+        console.log('Fields', fields);
+        console.log('Files', files);
+        // files.map(file => console.log(file));
+    });
+});
+
 app.get('/dest', (req, res) => {
     res.status(200).end();
-})
+});
 
 app.post('/score', (req, res) => {
     console.dir(req.headers);
@@ -26,6 +35,6 @@ app.post('/match', (req, res) => {
     });
 });
 
-app.listen(3000, () => {
-    console.log('Example app listening on port 3000!')
+app.listen(3333, () => {
+    console.log('Example app listening on port 3333!')
 });
